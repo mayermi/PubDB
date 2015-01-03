330b8c54423f58d3eae6274d3380b2e0f94d7920
kurzna <natalie-kurz1@gmx.de>
2015-01-03 23:20:07 +0100

    improvements in Authornetwork and timeline
    lines in network visible
    first version of timeline as barcharts

--- JS/author-network.js
+++ JS/author-network.js
@@ -22,8 +22,8 @@
     var link = svg.selectAll(".link")
       .data(graph.links)
       .enter().append("line")
-      .attr("class", "classLink")
-      .style("stroke-width", 2);
+      .attr("class", "link")
+      .style("stroke-width", function (d){return Math.sqrt(d.value); });
         
     var node = svg.selectAll(".node")
       .data(graph.nodes)
@@ -35,19 +35,50 @@
         })
       .call(force.drag);
       
+	 /* node.append("a")
+		.attr("xlink:href", "HTML/group.html");		//funktioniert nicht		*/
+	  
+	  
     node.append("title")
-      .text(function (d) {
-        return d.name;
-        });
+      .text(function (d) {return d.name;});
         
+		
     force.on("tick", function() {   //listen to updates in the computed layout positions
       link.attr("x1", function (d) { return d.source.x; })
       .attr("y1", function (d) { return d.source.y; })
-      .attr("x2", function (d) { return d.source.x; })
-      .attr("y2", function (d) {return d.source.y });
+      .attr("x2", function (d) { return d.target.x; })
+      .attr("y2", function (d) {return d.target.y });
       
     node.attr("cx", function (d) {return d.x;})
       .attr("cy", function (d) {return d.y});
       });
+	  
+	  
+	 //fisheye
+  
+	var fisheye = d3.fisheye.circular()
+      .radius(120)
+	  .distortion(2);
+	  
+	svg.on("mousemove", function(){
+		fisheye.focus(d3.mouse(this));
+		
+		node.each(function(d) { d.fisheye = fisheye(d); })
+		.attr("cx", function(d) { return d.fisheye.x; })
+		.attr("cy", function(d) { return d.fisheye.y; })
+		.attr("r", function(d) { return d.fisheye.z * 4.5; });
+
+		link.attr("x1", function(d) { return d.source.fisheye.x; })
+		.attr("y1", function(d) { return d.source.fisheye.y; })
+		.attr("x2", function(d) { return d.target.fisheye.x; })
+		.attr("y2", function(d) { return d.target.fisheye.y; });
+	});
+	  
+	  
   });
+  
+ 
+	  
+	
+  
 })();
